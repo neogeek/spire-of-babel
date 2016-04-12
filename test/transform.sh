cb360c4dc02dfad52b27f7e078e974028b27c1d6
Scott Doxey <neogeek0101@gmail.com>
2016-04-12 00:54:23 -0400

    Added output flag.

--- test/transform.sh
+++ test/transform.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 for file in test/fixture/*.js; do
-    ./bin/spire-of-babel "${file}" > test/fixture/transformed/"$(basename "$file")"
+    ./bin/spire-of-babel "${file}" --output test/fixture/transformed/"$(basename "$file")"
 done
