0c98c8218b49f841624a7f68e6fc25f5f82951ab
Scott Doxey <neogeek0101@gmail.com>
2016-08-15 23:42:41 -0400

    Added tests.

---
+++ test/specs/findESLintConfigFile.js
@@ -0,0 +1,52 @@
+'use strict';
+
+const utils = require('../../lib/utils');
+
+const TEST_ESLINT_PATH_REGEX = /test\/\.eslintrc$/;
+const PKG_ESLINT_PATH_REGEX = /\/((?!test).)*\/\.eslintrc/;
+
+describe('findESLintConfigFile', () => {
+
+    it('found config', (done) => {
+
+        utils.findESLintConfigFile('./test').then((path) => {
+
+            if (path.match(TEST_ESLINT_PATH_REGEX)) {
+
+                done();
+
+            }
+
+        });
+
+    });
+
+    it('found package config', (done) => {
+
+        utils.findESLintConfigFile('./').then((path) => {
+
+            if (path.match(PKG_ESLINT_PATH_REGEX)) {
+
+                done();
+
+            }
+
+        });
+
+    });
+
+    it('don\'t find config', (done) => {
+
+        utils.findESLintConfigFile('./bin').then((path) => {
+
+            if (path.match(PKG_ESLINT_PATH_REGEX)) {
+
+                done();
+
+            }
+
+        });
+
+    });
+
+});
