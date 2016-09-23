bfa7cddff6a98fb33c7cba77333e9c0d63d0f82e
Scott Doxey <neogeek0101@gmail.com>
2016-09-22 20:19:42 -0400

    Additional tests.

--- test/fixture/original/let.js
+++ test/fixture/original/let.js
@@ -1,5 +1,27 @@
+/* eslint id-length: 0 */
+/* eslint no-shadow: 0 */
+/* eslint no-undef: 0 */
+/* eslint no-undefined: 0 */
+/* eslint prefer-const: 0 */
+
 let greeting = 'Hello, friend.';
 
 greeting = 'Hello, friend?';
 
 console.log(greeting);
+
+{
+
+    let greeting = 'Hello, world.';
+
+    console.log(greeting);
+
+}
+
+for (let i = 0; i < 10; i += 1) {
+
+    console.log(i);
+
+}
+
+console.log(i === undefined);
--- test/fixture/transformed/let.js
+++ test/fixture/transformed/let.js
@@ -1,7 +1,27 @@
 'use strict';
 
+/* eslint id-length: 0 */
+/* eslint no-shadow: 0 */
+/* eslint no-undef: 0 */
+/* eslint no-undefined: 0 */
+/* eslint prefer-const: 0 */
+
 var greeting = 'Hello, friend.';
 
 greeting = 'Hello, friend?';
 
 console.log(greeting);
+
+{
+
+    var _greeting = 'Hello, world.';
+
+    console.log(_greeting);
+}
+
+for (var _i = 0; _i < 10; _i += 1) {
+
+    console.log(_i);
+}
+
+console.log(i === undefined);
