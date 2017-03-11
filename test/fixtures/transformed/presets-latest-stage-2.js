75411df1f4f835ba4017a32cee67f492818f5807
Scott Doxey <neogeek0101@gmail.com>
2017-03-11 16:34:16 -0500

    Added custom presets option.
    Added tests.
    Updated documentation.

---
+++ test/fixtures/transformed/presets-latest-stage-2.js
@@ -0,0 +1,28 @@
+'use strict';
+
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+var App = function () {
+    function App() {
+        _classCallCheck(this, App);
+
+        this.state = {
+            message: 'Hello, world!'
+        };
+    }
+
+    _createClass(App, [{
+        key: 'render',
+        value: function render() {
+            return React.createElement(
+                'p',
+                null,
+                this.state.message
+            );
+        }
+    }]);
+
+    return App;
+}();
