cba32c7fe43822243f4bccd46c43d13a5568c95a
neogeek <neogeek0101@gmail.com>
2019-07-17 22:26:53 -0400

    Updated packages.
    Updated fixtures.

--- test/fixtures/transformed/objects.js
+++ test/fixtures/transformed/objects.js
@@ -2,7 +2,11 @@
 
 var _interopRequireDefault = require("@babel/runtime/helpers/interopRequireDefault");
 
-var _objectSpread2 = _interopRequireDefault(require("@babel/runtime/helpers/objectSpread"));
+var _defineProperty2 = _interopRequireDefault(require("@babel/runtime/helpers/defineProperty"));
+
+function ownKeys(object, enumerableOnly) { var keys = Object.keys(object); if (Object.getOwnPropertySymbols) { var symbols = Object.getOwnPropertySymbols(object); if (enumerableOnly) symbols = symbols.filter(function (sym) { return Object.getOwnPropertyDescriptor(object, sym).enumerable; }); keys.push.apply(keys, symbols); } return keys; }
+
+function _objectSpread(target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i] != null ? arguments[i] : {}; if (i % 2) { ownKeys(source, true).forEach(function (key) { (0, _defineProperty2["default"])(target, key, source[key]); }); } else if (Object.getOwnPropertyDescriptors) { Object.defineProperties(target, Object.getOwnPropertyDescriptors(source)); } else { ownKeys(source).forEach(function (key) { Object.defineProperty(target, key, Object.getOwnPropertyDescriptor(source, key)); }); } } return target; }
 
 {
   var firstName = 'Scott';
@@ -31,7 +35,9 @@ var renderPolygon = function renderPolygon(points) {
     'lineWidth': 1,
     'strokeStyle': '#000'
   };
-  var settings = (0, _objectSpread2["default"])({}, defaults, options);
+
+  var settings = _objectSpread({}, defaults, {}, options);
+
   console.log(defaults);
   console.log(options);
   console.log(settings);
