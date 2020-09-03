44f4a5afb042d2257d2a48d81650b4475323c64e
Scott Doxey <neogeek0101@gmail.com>
2020-09-03 01:56:36 -0400

    Remove node call in fixture generate tool.

--- test/transform.sh
+++ test/transform.sh
@@ -4,14 +4,14 @@ rm test/fixtures/transformed/*.js
 
 for file in test/fixtures/original/*.js; do
     if [[ ! "${file}" =~ .bundle|sourcemap ]]; then
-        node ./bin/spire-of-babel "${file}" > ./test/fixtures/transformed/"$(basename "$file")"
+        ./bin/spire-of-babel.js "${file}" > ./test/fixtures/transformed/"$(basename "$file")"
     fi
 done
 
-node ./bin/spire-of-babel.js test/fixtures/original/bundle.js --bundle > ./test/fixtures/transformed/bundle.js
-node ./bin/spire-of-babel.js test/fixtures/original/sourcemap.js --sourcemap > ./test/fixtures/transformed/sourcemap_inline.js
-node ./bin/spire-of-babel.js test/fixtures/original/react.jsx > ./test/fixtures/transformed/react.js
+./bin/spire-of-babel.js test/fixtures/original/bundle.js --bundle > ./test/fixtures/transformed/bundle.js
+./bin/spire-of-babel.js test/fixtures/original/sourcemap.js --sourcemap > ./test/fixtures/transformed/sourcemap_inline.js
+./bin/spire-of-babel.js test/fixtures/original/react.jsx > ./test/fixtures/transformed/react.js
 
-node ./bin/spire-of-babel.js test/fixtures/original/typescript.ts > ./test/fixtures/transformed/typescript.js
+./bin/spire-of-babel.js test/fixtures/original/typescript.ts > ./test/fixtures/transformed/typescript.js
 
-NODE_ENV=production node ./bin/spire-of-babel.js test/fixtures/original/process_env_bundle.js --bundle > ./test/fixtures/transformed/process_env_bundle_production.js
+NODE_ENV=production ./bin/spire-of-babel.js test/fixtures/original/process_env_bundle.js --bundle > ./test/fixtures/transformed/process_env_bundle_production.js
